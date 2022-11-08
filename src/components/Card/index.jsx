99dd5888391f7a324b6b3e7dcf0487f1bb83d470
Sujay Alaspure <sujay.alaspure@robosoftin.com>
2022-11-08 21:14:58 +0530

    Added main card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from "react"
+import { BodyContent, CardBody, CardBodyTop, CardFooter, CardHeader, CardImage, CardWrapper } from "./style.card"
+import CloseIcon from "@mui/icons-material/Close"
+import Avatar from "../atoms/Avatar"
+import Title from "../atoms/Title"
+import VisibilityButton from "../VisibilityButton"
+import IconButton from "../atoms/IconButton"
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto"
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions"
+import Button from "../atoms/Button"
+import DisplayImage from "../atoms/DisplayImage"
+import TextArea from "../atoms/TextArea"
+
+function Card() {
+  const inputRef = useRef()
+  const [postData, setPostData] = useState({
+    content: "",
+    file: "blob:http://localhost:3000/2f0e180c-a98b-4e4e-bf3f-a51a6a5679ef",
+  })
+
+  const handlePostData = ({ type, data }) => {
+    setPostData((prev) => ({ ...prev, [type]: data }))
+  }
+
+  const handlePost = () => {
+    console.log({ ...postData, content: inputRef.current.value })
+  }
+
+  return (
+    <CardWrapper>
+      <CardHeader>
+        <Title name="Create a new post" />
+        <CloseIcon />
+      </CardHeader>
+
+      <CardBody>
+        <Avatar src="https://avatars.dicebear.com/api/pixel-art/ssa.svg?size=100" alt="random" />
+        <BodyContent>
+          <CardBodyTop>
+            <Title name="James Franco" />
+            <VisibilityButton />
+          </CardBodyTop>
+          <TextArea ref={inputRef} />
+          {postData.file && (
+            <DisplayImage onRemove={() => setPostData((prev) => ({ ...prev, file: "" }))} src={postData.file} />
+          )}
+        </BodyContent>
+      </CardBody>
+
+      <CardFooter>
+        <div className="action-holder">
+          <IconButton handleChange={handlePostData} type="file" Icon={InsertPhotoIcon} />
+          <IconButton type="emoji" Icon={EmojiEmotionsIcon} />
+        </div>
+        <Button onClick={handlePost} />
+      </CardFooter>
+    </CardWrapper>
+  )
+}
+
+export default Card
---
+++ src/components/Card/style.card.js
@@ -0,0 +1,76 @@
+import styled from "styled-components"
+import COLORS from "../../utils/Colors"
+
+export const CardWrapper = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+  /* padding: 16px; */
+  margin: 16px;
+  min-width: 500px;
+  min-height: 300px;
+  display: grid;
+  grid-template-rows: max-content auto max-content;
+`
+
+export const CardHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid #e7ebed;
+  /* margin-bottom: 0.2rem; */
+
+  .title {
+    font-size: 1.2 rem;
+  }
+`
+
+export const CardBody = styled.div`
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: max-content auto;
+  flex: 1;
+`
+
+export const BodyContent = styled.div`
+  padding-left: 1rem;
+
+  textarea {
+    width: 100%;
+    padding: 0.5rem;
+    border-radius: 4px;
+    border: none;
+    font-size: 18px;
+    color: ${COLORS.text};
+
+    &::placeholder {
+      color: #d1d6d9;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+`
+
+export const CardBodyTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const CardFooter = styled.div`
+  padding: 1rem 1.5rem;
+  border-top: 1px solid #e7ebed;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .action-holder {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+  }
+`
